f4446052b81ce9feb910b8e5c161ea278b5fe051
‹|| Λѕωιη ||› <82755354+Aswin-TG@users.noreply.github.com>
2021-09-12 17:55:08 +0530

    Update values.py

--- values.py
+++ values.py
@@ -3,7 +3,7 @@ API_HASH = "bc262c77441e719e3cfc2eb61a948e7f"
 SESSION = "AQB_bqP9O7TE1Si3lHefMpofUsk0eo8azfva_X4fU0yu1KG0J0rX1q8y78fd5Z28LpBNdkEBMrLY5z-eST29KL6oUyHf5jP6ZfsJurlKTolPPTeFO7NePQiK4oP-IgDCqhNJNGx78U9R-icuWjXcLzwOElnI6o0XZdCzcvXYbhxoysMDuDFe51myi2oT27FfAeaLteMJb4rqbN-RFRDhN4Xd4WX1Iaga6jNxEhM-ASqVNOuxt_GqDQGbgkLkNf5m8JLjtrLUz8Hyhjbf9l-Cbp0_401j4K0w5_bFDodGJ0c-52ojhi4ISG2MJtI8yCQpxpOyyhKbKwnaRTzxXde4q9TZdjKuqQA" 
 
 FROM_CHAT = -1001277083311
-TO_CHAT = -1001593777052
+TO_CHAT = -1001162523715
 
 FILE_TYPE = "video" # change as ur need [ photo, video, audio, document, photo_video ]
 CAPTION = "**[© Kerala Status Channel](https://t.me/KeralaStatusGroup)**"
