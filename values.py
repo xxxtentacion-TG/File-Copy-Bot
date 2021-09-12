520d80f43210c7015278bbc220d4b62e8afbc7ce
‹|| Λѕωιη ||› <82755354+Aswin-TG@users.noreply.github.com>
2021-09-12 17:15:02 +0530

    Update values.py

--- values.py
+++ values.py
@@ -1,6 +1,9 @@
-SESSION = "" 
-API_HASH = ""
-API_ID = 
+API_ID = 7259974
+API_HASH = "bc262c77441e719e3cfc2eb61a948e7f"
+SESSION = "AQB_bqP9O7TE1Si3lHefMpofUsk0eo8azfva_X4fU0yu1KG0J0rX1q8y78fd5Z28LpBNdkEBMrLY5z" 
 
-FROM_CHAT = 
-TO_CHAT = 
+FROM_CHAT = -1001211588305
+TO_CHAT = -1001593777052
+
+FILE_TYPE = "document" # change as ur need [ photo, video, audio, document, photo_video ]
+CAPTION = "**[© PSYCHO](https://t.me/Psych0_TG)**"
