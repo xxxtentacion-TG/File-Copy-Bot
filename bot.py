1ad06a1180cdf69fbebc386a371571a5c2180f0a
‹|| Λѕωιη ||› <82755354+Aswin-TG@users.noreply.github.com>
2021-09-12 17:17:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,16 +10,24 @@ bot = Client(SESSION
 @bot.on_message(filters.command("copy", "."))
 def copy_msg(client, message):
     m = message.reply("__Checking The Files...__")
-    for ids in bot.search_messages(chat_id=FROM_CHAT, filter=FILE_TYPE): 
-        msg_id = ids.message_id
-        idss.append(msg_id)
-        bot.copy_message(chat_id=TO_CHAT,from_chat_id=FROM_CHAT, message_id=msg_id, caption=CAPTION)
-    else:
-        if len(idss) == 0:
-            m.edit("__No Files To Copy...__")
-            return
-        else:
-            c = len(idss)
-            m.edit(f"__Copied {c} Messages To Channel__")
-            idss.clear() 
+    try:
+       for ids in bot.search_messages(chat_id=FROM_CHAT, filter=FILE_TYPE): 
+           msg_id = ids.message_id
+           idss.append(msg_id)
+           bot.copy_message(chat_id=TO_CHAT,
+                            from_chat_id=FROM_CHAT,
+                            message_id=msg_id, 
+                            caption=CAPTION)
+       else:
+           if len(idss) == 0:
+               m.edit("__No Files To Copy...__")
+               return
+           else:
+               c = len(idss)
+               m.edit(f"__Copied {c} Messages To Channel__")
+               idss.clear() 
+
+    except Exception as x
+       m.edit(f"Error Occurred: {x}")
+
 bot.run()
